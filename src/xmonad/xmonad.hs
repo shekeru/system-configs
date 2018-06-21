4ee953e7aa67b825a42bf2cafc0b75f71c9db2ec
Sheky Sheks <globallyjewed@gmail.com>
2018-06-21 11:49:53 +0000

    l

--- src/xmonad/xmonad.hs
+++ src/xmonad/xmonad.hs
@@ -5,15 +5,6 @@ import XMonad.Layout.Spacing
 import XMonad.Layout.Gaps
 import XMonad.Layout.SimpleFloat
 
-myTheme = defaultTheme { activeColor = "#f7a050",
-						 inactiveColor = "#ff8267",
-						 activeBorderColor = "#f7a050",
-						 inactiveBorderColor = "#ff8267",
-						 activeTextColor = "#24262b",
-						 inactiveTextColor = "#24262b",
-						 decoHeight = 24,
-						 fontName = "xft:Liberation Mono For Powerline:size=9" }
-
 myLayout = gaps [(U,50), (D,50), (L,75), (R,75)] $ spacing 10 $ emptyBSP
 
 main = xmonad $ defaultConfig
