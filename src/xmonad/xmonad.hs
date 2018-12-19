1d704c453002a1d2278002c06f0ceeb3530ba0f2
Sheky Sheks <sheks@desu.systems>
2018-12-19 06:40:35 +0000

    Update xmonad.hs

--- src/xmonad/xmonad.hs
+++ src/xmonad/xmonad.hs
@@ -10,6 +10,7 @@ myLayout = gaps [(U, 24), (D, 24), (L, 36), (R,36)] (spacing 10 emptyBSP)
 main = xmonad $ defaultConfig {
 	terminal = "urxvt",
 	layoutHook = myLayout,
+	modMask = mod4Mask,
 	borderWidth = 0,
 	normalBorderColor = "#f1f1f1",
 	focusedBorderColor = "#2970c4",
